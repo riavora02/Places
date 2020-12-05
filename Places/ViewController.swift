f43da6f696c8f329c5e52059175b5343ed8ce329
riavora02 <ria.vora02@gmail.com>
2020-12-05 17:46:22 -0500

    Initial Commit

---
+++ Places/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Places
+//
+//  Created by Ria Vora on 12/5/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
